5eaf2d7bd5cd9a6d7ee21e858cbc542acae0c15e
geenva <geenva@users.noreply.github.com>
2021-05-15 21:28:42 +0800

    fini

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,14 @@ struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
         if !msg.author.bot {
             if msg.content == "honk" {
                 if let Err(why) = msg.channel_id.say(&ctx.http, "honk").await {
+                    println!("Error sending message: {:?}", why);
+                }
+            } else if msg.content == "bonk" {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "bonk").await {
                     println!("Error sending message: {:?}", why);
                 }
             }
@@ -41,18 +40,11 @@ async fn main() {
     // Configure the client with your Discord bot token in the environment.
     let token = dotenv::var("TOKEN").expect("Please enter TOKEN in .env.");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .await
-        .expect("Err creating client");
+        .expect("Error creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
